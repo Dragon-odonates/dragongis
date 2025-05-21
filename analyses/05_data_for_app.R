ea069eccaa8dd33419e6920b9a9af70900412de4
Lisa Nicvert <lisa.nicvert@fondationbiodiversite.fr>
2025-05-21 16:17:42 +0200

    Fixed errors in data preparation for Shiny app

--- analyses/05_data_for_app.R
+++ analyses/05_data_for_app.R
@@ -5,8 +5,18 @@
 library(here)
 
 
+funcdir <- here("functions")
+source(here(funcdir, "functions.R"))
+
+res_grid <- 10000
+
 # Read data ---------------------------------------------------------------
 df <- readRDS(here::here("data", "derived-data", "occ_all_df.rds"))
+
+statrast <- readRDS(here("data", "derived-data", 
+                         paste0("stat_", res_grid, ".rds")))
+grid10 <- readRDS(here::here("data", "derived-data", 
+                             paste0("grid_", res_grid,".rds")))
 
 # Transform data ----------------------------------------------------------
 
@@ -17,54 +27,74 @@ mini <- df[, c("species", "dbID", "Year",
 mini <- mini[!duplicated(mini),]
 
 ## Compute spatial statistics by species -----------------------------
+ngrid <- table(df$grid10kmID)
+
+# Number of observation per cell
+obs_grid <- statrast[["nobs"]]
+nobs <- values(obs_grid)[, 1]
+# Number of year per cell
+year_grid <- statrast[["nyr"]]
+nyr <- values(statrast)[, 1]
+# Number of species per cell
+spe_grid <- statrast[["nspe"]]
+nspe <- values(spe_grid)[, 1]
+
+# Get species total counts
 n <- table(df$species)
-ngrid <- table(df$gridfac)
-nrast <- nspe
-prast <- nobs
-yrast <- nyr
-for (s in names(n)[n>50000]){
+
+for (s in names(n)[n > 50000]){
+  # Get the subset of observations for this species
   ms <- df$species == s
-  # percentage obs per grid cell
-  n_grid <- table(df$gridfac[ms])
-  n_grid[is.na(obs_grid)] <- NA
-  ns <- setValues(grid10, as.numeric(n_grid))
   
-  # percentage obs per grid cell
-  p_grid <- tapply(df$observationID[ms], df$gridfac[ms], nodup)/obs_grid*100
-  p_grid[is.na(obs_grid)] <- NA
-  p_grid[!is.na(obs_grid)&is.na(p_grid)] <- 0
+  # Number of obs of species s per grid cell
+  n_grid <- table(df$grid10kmID[ms])
+  n_grid[is.na(nobs)] <- NA
+  # Add to raster
+  ns <- setValues(grid10, as.numeric(nobs))
+  
+  # Percentage obs of species s contained in each grid cell
+  p_grid <- tapply(df$observationID[ms], df$grid10kmID[ms], nodup)/nobs*100
+  p_grid[is.na(nobs)] <- NA # Set percentage to zero of there were no obs of this species
+  # in sampled grid
+  p_grid[!is.na(nobs) & is.na(p_grid)] <- 0
+  # Add to raster
   ps <- setValues(grid10, as.numeric(p_grid))
   
-  # percentage year per grid cell
-  y_grid <- tapply(df$Year[ms], df$gridfac[ms], nodup)/year_grid*100
-  y_grid[is.na(obs_grid)] <- NA
-  y_grid[!is.na(obs_grid)&is.na(y_grid)] <- 0
+  # Percentage of years where s was observed in each grid cell
+  y_grid <- tapply(df$Year[ms], df$grid10kmID[ms], nodup)/nyr*100
+  y_grid[is.na(nobs)] <- NA
+  y_grid[!is.na(nobs)&is.na(y_grid)] <- 0
+  # Add to raster
   ys <- setValues(grid10, as.numeric(y_grid))
   
-  nrast <- c(nrast, ns)
-  names(nrast)[nlyr(nrast)] <- s
-  prast <- c(prast, ps)
+  # Add species raster to global raster
+  spe_grid <- c(spe_grid, ns)
+  names(spe_grid)[nlyr(spe_grid)] <- s
   
-  names(prast)[nlyr(prast)] <- s
-  yrast <- c(yrast, ys)
-  names(yrast)[nlyr(yrast)] <- s
+  obs_grid <- c(obs_grid, ps)
+  names(obs_grid)[nlyr(obs_grid)] <- s
+  
+  year_grid <- c(year_grid, ys)
+  names(year_grid)[nlyr(year_grid)] <- s
 }
 
 # Reproject
-sp <- project(nrast, crs("EPSG:3857"))
-names(sp)[1] <- "All"
-po <- project(prast, crs("EPSG:3857"))
-names(po)[1] <- "All"
-py <- project(yrast, crs("EPSG:3857"))
-names(py)[1] <- "All"
+spe_grid <- project(spe_grid, crs("EPSG:3857"))
+names(spe_grid)[1] <- "All"
+
+obs_grid <- project(obs_grid, crs("EPSG:3857"))
+names(obs_grid)[1] <- "All"
+
+year_grid <- project(year_grid, crs("EPSG:3857"))
+names(year_grid)[1] <- "All"
 
 
 # Export to app -----------------------------------------------------------
 saveRDS(mini, here::here("analyses", "app", "data", "occ_mini.rds"))
 
-saveRDS(sp, 
+saveRDS(spe_grid, 
         here::here("analyses", "app", "data", "sp10000.rds"), compress="xz")
-saveRDS(po, 
+saveRDS(obs_grid, 
         here::here("analyses", "app", "data", "po10000.rds"), compress="xz")
-saveRDS(py, 
+saveRDS(year_grid, 
         here::here("analyses", "app", "data", "py10000.rds"), compress="xz")
